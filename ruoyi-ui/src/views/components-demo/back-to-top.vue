2ad7af0a36fe1cc65d90c8e744e43fe867008b45
taoqibao123 <483822335@qq.com>
2020-08-28 16:47:11 +0800

    全局返回顶部展示

---
+++ ruoyi-ui/src/views/components-demo/back-to-top.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="app-container">
+    <div class="real-container">
+      <aside>
+        全局返回顶部代码在：src\layout\index.vue(el-backtop标签)
+      </aside>
+      <div v-for="(item, index) in 100" :key="index" style="padding: 20px;">
+        测试滚轮显示返回顶部-{{ index }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BackToTopDemo',
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style scoped></style>
