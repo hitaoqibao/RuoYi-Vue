a35ea4a3632c1d786a206590d6ce24ccfcf79f49
chengkang.bao 包承康 <chengkang.bao@cminl.oa>
2020-08-31 08:29:56 +0800

    音乐播放器

---
+++ ruoyi-ui/src/views/components-demo/music.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="app-container">
+    <div class="real-container">
+      <el-divider content-position="left">
+        基于
+        <a class="link-type" href="https://www.npmjs.com/package/zw-player"
+          >zw-player</a
+        >
+        音乐播放器
+      </el-divider>
+      <player> </player>
+    </div>
+  </div>
+</template>
+
+<script>
+import Player from 'zw-player';
+export default {
+  data() {
+    return {};
+  },
+  components: { Player },
+};
+</script>
+
+<style></style>
