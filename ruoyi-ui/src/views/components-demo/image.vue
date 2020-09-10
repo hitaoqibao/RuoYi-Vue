b6a1d1528a8dd1bb5e787a9423cd66669ecd0d36
chengkang.bao 包承康 <chengkang.bao@cminl.oa>
2020-09-10 19:04:45 +0800

    卡片特效

---
+++ ruoyi-ui/src/views/components-demo/image.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="app-container">
+    <image-effect
+      :url="require('@/assets/image/carousel-image/image1.jpg')"
+    ></image-effect>
+  </div>
+</template>
+
+<script>
+import ImageEffect from "@/components/ImageEffect";
+export default {
+  components: {
+    ImageEffect
+  },
+  data() {
+    return {};
+  }
+};
+</script>
+
+<style></style>
