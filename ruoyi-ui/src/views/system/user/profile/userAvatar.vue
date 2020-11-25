fdbf1707abefd96763fa3dc5c7f2fdfd630d83bd
hitaoqibao <483822335@qq.com>
2020-11-25 17:31:30 +0800

    优化头像样式，鼠标移入悬停遮罩

--- ruoyi-ui/src/views/system/user/profile/userAvatar.vue
+++ ruoyi-ui/src/views/system/user/profile/userAvatar.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-    <img v-bind:src="options.img" @click="editCropper()" title="点击上传头像" class="img-circle img-lg" />
+    <div class="user-info-head" @click="editCropper()">
+      <img
+        v-bind:src="options.img"
+        title="点击上传头像"
+        class="img-circle img-lg"
+      />
+    </div>
     <el-dialog
       :title="title"
       :visible.sync="open"
@@ -9,7 +15,7 @@
       @opened="modalOpened"
     >
       <el-row>
-        <el-col :xs="24" :md="12" :style="{height: '350px'}">
+        <el-col :xs="24" :md="12" :style="{ height: '350px' }">
           <vue-cropper
             ref="cropper"
             :img="options.img"
@@ -22,7 +28,7 @@
             v-if="visible"
           />
         </el-col>
-        <el-col :xs="24" :md="12" :style="{height: '350px'}">
+        <el-col :xs="24" :md="12" :style="{ height: '350px' }">
           <div class="avatar-upload-preview">
             <img :src="previews.url" :style="previews.img" />
           </div>
@@ -43,20 +49,38 @@
             </el-button>
           </el-upload>
         </el-col>
-        <el-col :lg="{span: 1, offset: 2}" :md="2">
-          <el-button icon="el-icon-plus" size="small" @click="changeScale(1)"></el-button>
+        <el-col :lg="{ span: 1, offset: 2 }" :md="2">
+          <el-button
+            icon="el-icon-plus"
+            size="small"
+            @click="changeScale(1)"
+          ></el-button>
         </el-col>
-        <el-col :lg="{span: 1, offset: 1}" :md="2">
-          <el-button icon="el-icon-minus" size="small" @click="changeScale(-1)"></el-button>
+        <el-col :lg="{ span: 1, offset: 1 }" :md="2">
+          <el-button
+            icon="el-icon-minus"
+            size="small"
+            @click="changeScale(-1)"
+          ></el-button>
         </el-col>
-        <el-col :lg="{span: 1, offset: 1}" :md="2">
-          <el-button icon="el-icon-refresh-left" size="small" @click="rotateLeft()"></el-button>
+        <el-col :lg="{ span: 1, offset: 1 }" :md="2">
+          <el-button
+            icon="el-icon-refresh-left"
+            size="small"
+            @click="rotateLeft()"
+          ></el-button>
         </el-col>
-        <el-col :lg="{span: 1, offset: 1}" :md="2">
-          <el-button icon="el-icon-refresh-right" size="small" @click="rotateRight()"></el-button>
+        <el-col :lg="{ span: 1, offset: 1 }" :md="2">
+          <el-button
+            icon="el-icon-refresh-right"
+            size="small"
+            @click="rotateRight()"
+          ></el-button>
         </el-col>
-        <el-col :lg="{span: 2, offset: 6}" :md="2">
-          <el-button type="primary" size="small" @click="uploadImg()">提 交</el-button>
+        <el-col :lg="{ span: 2, offset: 6 }" :md="2">
+          <el-button type="primary" size="small" @click="uploadImg()"
+            >提 交</el-button
+          >
         </el-col>
       </el-row>
     </el-dialog>
@@ -67,13 +91,13 @@
 import store from "@/store";
 import { VueCropper } from "vue-cropper";
 import { uploadAvatar } from "@/api/system/user";
-
+// 修改头像
 export default {
   components: { VueCropper },
   props: {
     user: {
-      type: Object,
-    },
+      type: Object
+    }
   },
   data() {
     return {
@@ -88,9 +112,9 @@ export default {
         autoCrop: true, // 是否默认生成截图框
         autoCropWidth: 200, // 默认生成截图框宽度
         autoCropHeight: 200, // 默认生成截图框高度
-        fixedBox: true, // 固定截图框大小 不允许改变
+        fixedBox: true // 固定截图框大小 不允许改变
       },
-      previews: {},
+      previews: {}
     };
   },
   methods: {
@@ -131,10 +155,10 @@ export default {
     },
     // 上传图片
     uploadImg() {
-      this.$refs.cropper.getCropBlob((data) => {
+      this.$refs.cropper.getCropBlob(data => {
         let formData = new FormData();
         formData.append("avatarfile", data);
-        uploadAvatar(formData).then((response) => {
+        uploadAvatar(formData).then(response => {
           if (response.code === 200) {
             this.open = false;
             this.options.img = process.env.VUE_APP_BASE_API + response.imgUrl;
@@ -148,7 +172,33 @@ export default {
     // 实时预览
     realTime(data) {
       this.previews = data;
-    },
-  },
+    }
+  }
 };
 </script>
+
+<style scoped lang="scss">
+.user-info-head {
+  position: relative;
+  display: inline-block;
+  height: 120px;
+}
+
+.user-info-head:hover:after {
+  content: "+";
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  color: #eee;
+  background: rgba(0, 0, 0, 0.5);
+  font-size: 24px;
+  font-style: normal;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  cursor: pointer;
+  line-height: 110px;
+  border-radius: 50%;
+}
+</style>
